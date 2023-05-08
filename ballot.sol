bfe121081a808a26454fe361cc984c9e770e493d
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2023-05-08 20:22:06 +0530

    concated encryption

--- ballot.sol
+++ ballot.sol
@@ -1,71 +1,73 @@
-/**
- * @file ballot.sol
- * @author Jackson Ng <jackson@jacksonng.org>
- * @date created 22nd Apr 2019
- * @date last modified 30th Apr 2019
- */
-
 pragma solidity ^0.5.0;
 
 contract Ballot {
-
-    struct vote{
+    struct vote {
         address voterAddress;
         string choice;
     }
-    
-    struct voter{
+
+    struct voter {
         string voterName;
         bool voted;
     }
 
-    uint private countResult = 0;
-    uint public finalResult = 0;
-    uint public totalVoter = 0;
-    uint public totalVote = 0;
-    address public ballotOfficialAddress;      
+    uint256 private countResult = 0;
+    uint256 public finalResult = 0;
+    uint256 public totalVoter = 0;
+    uint256 public totalVote = 0;
+    address public ballotOfficialAddress;
     string public ballotOfficialName;
     string public proposal;
-    bytes32 private messageHash;
-    
-    mapping(uint => vote) private votes;
+    string public hashed;
+    string public hashedd;
+    string public msg_sec;
+    string public choice;
+    bytes32 public _bytes32;
+    bytes32 public temp;
+    bytes public result2;
+    bytes public result;
+
+    mapping(uint256 => vote) private votes;
     mapping(address => voter) public voterRegister;
-    
-    enum State { Created, Voting, Ended }
-	State public state;
-	
-	//creates a new ballot contract
-	constructor(
-        string memory _ballotOfficialName,
-        string memory _proposal) public {
+
+    enum State {
+        Created,
+        Voting,
+        Ended
+    }
+    State public state;
+
+    //creates a new ballot contract
+    constructor(string memory _ballotOfficialName, string memory _proposal)
+        public
+    {
         ballotOfficialAddress = msg.sender;
         ballotOfficialName = _ballotOfficialName;
         proposal = _proposal;
-        
+
         state = State.Created;
     }
-    
-    
-	modifier condition(bool _condition) {
-		require(_condition);
-		_;
-	}
 
-	modifier onlyOfficial() {
-		require(msg.sender ==ballotOfficialAddress);
-		_;
-	}
+    modifier condition(bool _condition) {
+        require(_condition);
+        _;
+    }
 
-	modifier inState(State _state) {
-		require(state == _state);
-		_;
-	}
+    modifier onlyOfficial() {
+        require(msg.sender == ballotOfficialAddress);
+        _;
+    }
+
+    modifier inState(State _state) {
+        require(state == _state);
+        _;
+    }
 
     event voterAdded(address voter);
     event voteStarted();
-    event voteEnded(uint finalResult);
+    event voteEnded(uint256 finalResult);
     event voteDone(address voter);
-    
+
     //add voter
     function addVoter(address _voterAddress, string memory _voterName)
         public
@@ -81,18 +83,9 @@ contract Ballot {
     }
 
     //declare voting starts now
-    function startVote()
-        public
-        inState(State.Created)
-        onlyOfficial
-    {
-        state = State.Voting;     
+    function startVote() public inState(State.Created) onlyOfficial {
+        state = State.Voting;
         emit voteStarted();
-    }
-
-    // Function to get hash value
-    function getMessageHash() public view returns (bytes32) {
-        return messageHash;
     }
 
     //voters vote by indicating their choice (true/false)
@@ -102,19 +95,49 @@ contract Ballot {
         returns (bool voted)
     {
         bool found = false;
-        
-        if (bytes(voterRegister[msg.sender].voterName).length != 0 
-        && !voterRegister[msg.sender].voted){
+
+        if (
+            bytes(voterRegister[msg.sender].voterName).length != 0 &&
+            !voterRegister[msg.sender].voted
+        ) {
             voterRegister[msg.sender].voted = true;
             vote memory v;
             v.voterAddress = msg.sender;
-            v.choice = _choice;
-            if (keccak256(abi.encodePacked("6273151f959616268004b58dbb21e5c851b7b8d04498b4aabee12291d22fc034 ")) == keccak256(abi.encodePacked(_choice))){
-                messageHash = keccak256(bytes(_choice));
-                countResult++; //counting on the go
+
+            bytes memory strBytes = bytes(_choice);
+            result = new bytes(5);
+            for (uint256 i = 0; i < 5; i++) {
+                result[i] = strBytes[i];
             }
-            else if (keccak256(abi.encodePacked("ba9154e0baa69c78e0ca563b867df81bae9d177c4ea1452c35c84386a70f0f7a")) == keccak256(abi.encodePacked(_choice))){
-                messageHash = keccak256(bytes(_choice));
+            v.choice = string(result);
+            choice = string(result);
+
+            result2 = new bytes((bytes(_choice).length) - 5);
+            for (uint256 j = 5; j < (bytes(_choice).length); j++) {
+                choice = "blah";
+                result2[j - 5] = strBytes[j];
+            }
+            hashed = string(result2);
+
+            msg_sec = string(abi.encodePacked(v.choice, "SECRET"));
+
+            temp = (keccak256(abi.encodePacked(msg_sec)));
+            bytes memory bytesArray = new bytes(32);
+            for (uint256 i; i < 32; i++) {
+                bytesArray[i] = _bytes32[i];
+            }
+            hashedd = string(bytesArray);
+
+            if (
+                keccak256(abi.encodePacked(hashed)) ==
+                keccak256(abi.encodePacked(temp))
+            ) {
+                if (
+                    (keccak256(abi.encodePacked("ttrue")) ==
+                        keccak256(abi.encodePacked(v.choice)))
+                ) {
+                    countResult++; //counting on the go
+                }
             }
             votes[totalVote] = v;
             totalVote++;
@@ -123,13 +146,9 @@ contract Ballot {
         emit voteDone(msg.sender);
         return found;
     }
-    
+
     //end votes
-    function endVote()
-        public
-        inState(State.Voting)
-        onlyOfficial
-    {
+    function endVote() public inState(State.Voting) onlyOfficial {
         state = State.Ended;
         finalResult = countResult; //move result from private countResult to public finalResult
         emit voteEnded(finalResult);
